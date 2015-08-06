54b5cf2cdbe2719a79505d36b3ce463d474ac163
Rasel Ahmmed Bappi <raselahmmed@hotmail.com>
2015-08-06 16:54:54 +0600

    2015-08-06

--- lab.browserinfo/lab.browserinfo/Program.cs
+++ lab.browserinfo/lab.browserinfo/Program.cs
@@ -1,20 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using lab.errorlogapps.Models;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace lab.errorlogapps.Controllers
+namespace lab.browserinfo
 {
-    public class HomeController : Controller
+    class Program
     {
-        readonly log4net.ILog _logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-        private AppDbContext _db = new AppDbContext();
-        public ActionResult Index()
+        static void Main(string[] args)
         {
-            HttpBrowserCapabilitiesBase browser = Request.Browser;
-            string browserInfo = "Browser Capabilities\n"
+            Console.WriteLine("Start");
+
+            System.Web.HttpBrowserCapabilities browser = System.Web.HttpContext.Current.Request.Browser;
+            string s = "Browser Capabilities\n"
                 + "Type = " + browser.Type + "\n"
                 + "Name = " + browser.Browser + "\n"
                 + "Version = " + browser.Version + "\n"
@@ -38,32 +37,8 @@ namespace lab.errorlogapps.Controllers
                 + "Supports JavaScript Version = " +
                     browser["JavaScriptVersion"] + "\n";
 
-
-
-            //var categoryList = _db.Category.ToList();
-            _logger.Info("Starting GenerateDebugLog Method...");
-            _logger.Debug("No input parameters for method.");
-
-            _logger.Info("Starting loop...");
-            for (int i = 0; i <= 10; i++)
-            {
-                _logger.Debug("Current value: " + i);
-            }
-            _logger.Info("End loop.");
-
-            _logger.Info("End GenerateDebugLog Method.");
-
-            try
-            {
-                throw new Exception("Exception test!");
-            }
-            catch (Exception ex)
-            {
-                _logger.Error("Error! ", ex);
-            }
-
-            return View();
+            Console.WriteLine(s);
+            Console.ReadLine();
         }
-
     }
 }
