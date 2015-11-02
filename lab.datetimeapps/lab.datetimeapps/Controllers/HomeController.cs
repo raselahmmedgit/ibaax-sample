d37788f689602f1e1e73fdf49bf73223c5af3b27
Rasel Ahmmed Bappi <raselahmmed@hotmail.com>
2015-11-02 16:26:54 +0600

    2015-11-02

--- lab.datetimeapps/lab.datetimeapps/Controllers/HomeController.cs
+++ lab.datetimeapps/lab.datetimeapps/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using lab.datetimeapps.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,11 +14,63 @@ namespace lab.datetimeapps.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult CategoryList()
         {
-            ViewBag.Message = "Your application description page.";
+            var categories = new List<Category>
+                            {
+                                new Category { CategoryId=1, Name = "Fruit", CreateDate = DateTime.Now},
+                                new Category {CategoryId=2, Name = "Cloth", CreateDate = DateTime.Now},
+                                new Category {CategoryId=3, Name = "Car", CreateDate = DateTime.Now},
+                                new Category {CategoryId=4, Name = "Book", CreateDate = DateTime.Now},
+                                new Category {CategoryId=5, Name = "Shoe", CreateDate = DateTime.Now},
+                                new Category {CategoryId=6, Name = "Wiper", CreateDate = DateTime.Now},
+                                new Category {CategoryId=7, Name = "Laptop", CreateDate = DateTime.Now},
+                                new Category {CategoryId=8, Name = "Desktop", CreateDate = DateTime.Now},
+                                new Category {CategoryId=9, Name = "Notebook", CreateDate = DateTime.Now},
+                                new Category {CategoryId=10, Name = "AC", CreateDate = DateTime.Now}
 
-            return View();
+                            };
+
+            return View(categories.ToList());
+        }
+
+        public ActionResult PDFByRazorPDF()
+        {
+            var categories = new List<Category>
+                            {
+                                new Category { CategoryId=1, Name = "Fruit", CreateDate = DateTime.Now},
+                                new Category {CategoryId=2, Name = "Cloth", CreateDate = DateTime.Now},
+                                new Category {CategoryId=3, Name = "Car", CreateDate = DateTime.Now},
+                                new Category {CategoryId=4, Name = "Book", CreateDate = DateTime.Now},
+                                new Category {CategoryId=5, Name = "Shoe", CreateDate = DateTime.Now},
+                                new Category {CategoryId=6, Name = "Wiper", CreateDate = DateTime.Now},
+                                new Category {CategoryId=7, Name = "Laptop", CreateDate = DateTime.Now},
+                                new Category {CategoryId=8, Name = "Desktop", CreateDate = DateTime.Now},
+                                new Category {CategoryId=9, Name = "Notebook", CreateDate = DateTime.Now},
+                                new Category {CategoryId=10, Name = "AC", CreateDate = DateTime.Now}
+
+                            };
+            //return View(categories.ToList());
+            return new RazorPDF.PdfResult(categories.ToList(), "CategoryList");
+        }
+
+        public ActionResult PDFByRotativa()
+        {
+            var categories = new List<Category>
+                            {
+                                new Category { CategoryId=1, Name = "Fruit", CreateDate = DateTime.Now},
+                                new Category {CategoryId=2, Name = "Cloth", CreateDate = DateTime.Now},
+                                new Category {CategoryId=3, Name = "Car", CreateDate = DateTime.Now},
+                                new Category {CategoryId=4, Name = "Book", CreateDate = DateTime.Now},
+                                new Category {CategoryId=5, Name = "Shoe", CreateDate = DateTime.Now},
+                                new Category {CategoryId=6, Name = "Wiper", CreateDate = DateTime.Now},
+                                new Category {CategoryId=7, Name = "Laptop", CreateDate = DateTime.Now},
+                                new Category {CategoryId=8, Name = "Desktop", CreateDate = DateTime.Now},
+                                new Category {CategoryId=9, Name = "Notebook", CreateDate = DateTime.Now},
+                                new Category {CategoryId=10, Name = "AC", CreateDate = DateTime.Now}
+
+                            };
+            return new Rotativa.ViewAsPdf("CategoryList", categories.ToList()) { FileName = "CategoryList.pdf" };
         }
 
         public ActionResult Contact()
